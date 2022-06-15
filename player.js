a13939b70da3f6c4c2456b9ac0bf787608a55ce1
Kristian Freeman <kristian@kristianfreeman.com>
2022-06-15 16:35:21 +0100

    Fix stream render URL

--- player.js
+++ player.js
@@ -7,7 +7,7 @@ function initializePlayer() {
   document.getElementById("player-container").style.display = "block";
   const videoId = path.replace("/video/", "");
 
-  document.getElementById("embed-container").innerHTML = '<iframe id="stream-player" src="https://iframe.videodelivery.net/' + videoId + '" style="display:none;border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" allowfullscreen="true"></iframe>';
+  document.getElementById("embed-container").innerHTML = '<iframe id="stream-player" src="https://iframe.cloudflarestream.com/' + videoId + '" style="display:none;border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" allowfullscreen="true"></iframe>';
   const player = Stream(document.getElementById('stream-player'));
   player.addEventListener('error', () => {
     document.getElementById("stream-player").style.display = "none";
